ee69b9a975d86d3ceda5dc27fcf781114c1bfc12
minhhoccode111 <minhhoccode111@gmail.com>
2023-03-18 01:19:13 +0700

    changes

--- theOdinProject/objectsAndObjectConstructors/app.js
+++ theOdinProject/objectsAndObjectConstructors/app.js
@@ -1,10 +1,10 @@
 "use strict";
 // console.log("Hello world from app.js");
 function Book(title, author, pages, read) {
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.read = read);
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 Book.prototype.info = function () {
   return `
