abfafedc2d1b68a6e18cc05c130ef391070ec13a
minhhoccode111 <minhhoccode111@gmail.com>
2023-03-18 04:29:59 +0700

    some lessons on prototypical inheritance in JavaScript

--- theOdinProject/objectsAndObjectConstructors/app.js
+++ theOdinProject/objectsAndObjectConstructors/app.js
@@ -1,23 +1,69 @@
 "use strict";
-// console.log("Hello world from app.js");
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-Book.prototype.info = function () {
-  return `
-  This book has Title: ${this.title} is written by ${this.author} and has ${this.page} pages. Read is ${this.read}
-  `;
-};
-const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 600, true);
-// console.log(book1.info());
+// // console.log("Hello world from app.js");
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+// Book.prototype.info = function () {
+//   return `
+//   This book has Title: ${this.title} is written by ${this.author} and has ${this.page} pages. Read is ${this.read}
+//   `;
+// };
+// const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 600, true);
+// // console.log(book1.info());
 
-let x = {};
-let xR = Object.getPrototypeOf(x);
-let xP = x.__proto__;
-console.log(x);
-console.log(xR);
-console.log(xP);
-console.log(Object.getPrototypeOf(Object.prototype));
+// let x = {};
+// let xR = Object.getPrototypeOf(x);
+// let xP = x.__proto__;
+// console.log(x);
+// console.log(xR);
+// console.log(xP);
+// console.log(Object.getPrototypeOf(Object.prototype));
+
+// Initialize a constructor function for a new Hero
+function Hero(name, level) {
+  this.name = name;
+  this.level = level;
+}
+// We have created a constructor function called Hero with two parameters: name and level. Since every character will have a name and a level, it makes sense for each new character to have these properties. The this keyword will refer to the new instance that is created, so setting this.name to the name parameters ensures the new object will have a name property set.
+//Now we can create a new instance with new keyword
+let hero1 = new Hero("Bjorn", 1);
+//If we console out hero1, we will see a new object has been created with the new properties set as expected
+console.log(hero1); //Hero {name:'Bjorn', level:1}
+//Now if we get the [[Prototype]] of hero1, we will be able to see the constructor as Hero(). Remember, this has the same input as hero1__proto__, but is the proper method to use
+Object.getPrototypeOf(hero1); //constructor: ƒ Hero(name, level)
+//You may notice that we've only defined properties and not methods in the constructor. It is a common practice in JavaScript to define methods on the prototype for increased efficiency and code readability
+//We can add a method to Hero using prototype. We'll create a greet() method
+//Add greet method to the Hero prototype
+Hero.prototype.greet = function () {
+  return `${this.name} say hello.`;
+};
+//Since greet() is in the prototype of Hero, and hero1 in an instance of Hero, the method is available to hero1
+hero1.greet(); //Bjorn say hello
+//if you inspect the [[Prototype]] of Hero, you will see greet() as an available option now.
+//This is good, but now we want to create classes for the heroes to use. It wouldn't make sense to put all the abilities for every class into the Hero constructor, because different classes will have different abilities. We want to create new constructor functions, but we also want them to be connected to the original Hero.
+//We can use the call() method to copy over properties from one constructor into another constructor. Let's create a Warrior and a Healer constructor.
+//Initialize Warrior constructor
+function Warrior(name, level, weapon) {
+  //chain constructor with call
+  Hero.call(this, name, level);
+  //add new property
+  this.weapon = weapon;
+}
+
+//Initialize Healer constructor
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+  this.spell = spell;
+}
+
+//Both new constructor now have the properties of Hero and a few unique ones. We'll add the attack() method to Warrior, and the heal() method to Healer
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attack with ${this.weapon}`;
+};
+Healer.prototype.heal = function () {
+  return `${this.name} casts ${this.spell}`;
+};
--- theOdinProject/objectsAndObjectConstructors/text.md
+++ theOdinProject/objectsAndObjectConstructors/text.md
@@ -1,1 +1,11 @@
 ## Use this to take notes
+
+### Constructor Functions
+
+- <strong>Constructor Functions</strong> are functions that are used to construct new objects. The <code>new</code> operator is used to create new instances based off a constructor function. We have seen some built-in JavaScript constructors,such as new Array(), new Object(), new String() but we can also create out own custom templates from which to build new objects
+- As an example, let's say we are creating a very simple, text-based role-playing game. A user can select a character and then choose what character class they will have, such as warrior healer, thief and so on
+- Since each characters will share many characteristics, such as having a name, a level, and hit points, it makes sense to create a constructor as a template. However, since each character class may have vastly different abilities, we want to make sure each character only has access to their own abilitites. Let's take a look at how we can accomplish this with prototype inheritance and constructors.
+- To begin, a constructor function is just a regular function. It becomes a constructor when it is called on by an instance with the new keyword. In JavaScript, we capitalize the first letter of a constructor function by convention.
+- We have created a constructor function called Hero with two parameters: name and level. Since every character will have a name and a level, it makes sense for each new character to have these properties. The this keyword will refer to the new instance that is created, so setting this.name to the name parameter ensures the new object will have a name property set
+- now we can create a new instance with new
+- Now if we get the [[Prototype]] of hero1, we will be able to see the constructor as Hero(). (Remember this has the same input as hero1.**proto**, but is the proper method to use)
