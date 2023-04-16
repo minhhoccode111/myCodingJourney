a82f3433f9f201ac515df26e81f9a08fbf8535cb
minhhoccode <minhhoccode111@gmail.com>
2023-04-16 10:44:19 +0700

    this keyword and factory function

--- theOdinProject/factoryFunstionsAndTheModulePattern/theOdinProjectArticle.js
+++ theOdinProject/factoryFunstionsAndTheModulePattern/theOdinProjectArticle.js
@@ -35,3 +35,37 @@ dummyFactory.prototype.introduction = function () {
 // console.log(dFac); //object with name = "Minh" and age = 23
 // console.log(dFac.type); //Human (on prototype)
 dFac.introduction();
+
+//KNOW ABOUT "THIS" KEYWORD
+let myFunction = function () {
+  return this; //this=global, [object Window]
+};
+// console.log(myFunction());//Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+
+let myObject = {
+  myMethod: function () {
+    return this; //this=myObject, [Object myObject]
+  },
+};
+// console.log(myObject.myMethod()); //{myMethod: ƒ}
+
+const button = document.getElementById("showThis");
+button.addEventListener("click", function (e) {
+  console.log(this); //this=<button id="showThis">Click to show "this" value</button>
+  console.log(e.target); //e.target=<button id="showThis">Click to show "this" value</button>
+
+  //this===e.target when we use "function" keyword
+
+  const arrowInFunction = () => {
+    console.log(this); //<button id="showThis">Click to show "this" value</button>
+  };
+  arrowInFunction();
+});
+
+button.addEventListener("mouseenter", (e) => {
+  console.log(this); // window object
+  //this===Window{} when we use arrow function
+});
+
+//PRIVATE AND PUBLIC SCOPE
+//
