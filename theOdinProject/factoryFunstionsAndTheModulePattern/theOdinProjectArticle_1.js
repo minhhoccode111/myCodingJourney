3626dac7345665fcd2baba4b03c344f59ddbc6b3
minhhoccode <minhhoccode111@gmail.com>
2023-04-17 11:23:42 +0700

    more about destructuring

--- theOdinProject/factoryFunstionsAndTheModulePattern/theOdinProjectArticle_1.js
+++ theOdinProject/factoryFunstionsAndTheModulePattern/theOdinProjectArticle_1.js
@@ -1,26 +1,45 @@
-//MORE ABOUT JAVASCRIPT DESTRUCTURING
 // console.log("Hello");
-
+//MORE ABOUT JAVASCRIPT DESTRUCTURING
+//ARR
 const arr = [1, 2, 3, 4, 5];
 const { ...a } = arr;
-console.log(a);
+// console.log(a);
+
+//OBJ
+const obj = {
+  firstName: "Hoang",
+  lastName: "Minh",
+  name: "Hoang Minh",
+  age: 22,
+  gender: "male",
+  educated: true,
+  level: "Junior",
+  languages: ["javaScript", "HTML", "CSS", "Nodejs"],
+};
+//basic assignment
+const { lastName, age } = obj;
+console.log(lastName, age); //Minh 22
+
+//assigning to new variable names
+const { lastName: lastNameInObj, age: ageInObj } = obj;
+console.log(lastNameInObj, ageInObj); //Minh 22
 
 //INHERITANCE WITH FACTORIES FUNCTION IN JAVASCRIPT WITH DESTRUCTURING
-const People = (name) => {
+function People(name) {
   const getName = () => name;
   const intro = () => `Hi, my name is ${name}`;
   return { getName, intro };
-};
+}
 
 const me = People("Minh");
 const you = People("Hoang");
-console.log(me);
-console.log(me.intro());
-console.log(me.getName());
+// console.log(me);
+// console.log(me.intro());
+// console.log(me.getName());
 
-console.log(you);
-console.log(you.intro());
-console.log(you.getName());
+// console.log(you);
+// console.log(you.intro());
+// console.log(you.getName());
 
 const Programmer = (name, level) => {
   const { intro } = People(name);
@@ -29,9 +48,9 @@ const Programmer = (name, level) => {
 };
 
 const minh = Programmer("Minh", "Junior");
-console.log(minh);
-console.log(minh.intro()); //Hi, my name is Minh
-console.log(minh.greeting()); // Hi, I am Minh, Junior developer.
+// console.log(minh);
+// console.log(minh.intro()); //Hi, my name is Minh
+// console.log(minh.greeting()); // Hi, I am Minh, Junior developer.
 
 //INHERITANCE WITH FACTORIES FUNCTION IN JAVASCRIPT WITH Object.assign()
 
@@ -44,8 +63,8 @@ const Nerd = (name, level) => {
 };
 
 const hoang = Nerd("Hoang", "Senior");
-console.log(hoang);
-console.log(hoang.doSomeNerdy());
-console.log(hoang.getName());
-console.log(hoang.greeting());
-console.log(hoang.intro());
+// console.log(hoang);
+// console.log(hoang.doSomeNerdy());
+// console.log(hoang.getName());
+// console.log(hoang.greeting());
+// console.log(hoang.intro());
